e331d25b39f9c298a7e668773dcae9382cff3ce1
rhuanpedrero <117355001+rhuanpedrero@users.noreply.github.com>
2024-06-10 11:50:39 -0300

    Update main.js

--- area de trabalho 2/main.js
+++ area de trabalho 2/main.js
@@ -40,15 +40,17 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-function atualizaCronometro(){
+function atualizaCronometro()
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);  
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
+       
+    } if (tempoFinal > 0){
+        return [dias,horas,minutos,segundos];
+    } else {
+        return [0,0,0,0];
     }
-}
-
-function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
-}
-
-comecaCronometro();
